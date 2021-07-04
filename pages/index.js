c333f79fdcf0f2082740449cf122cb84f2910bed
Thomas13702 <thomaspritchard137@btinternet.com>
2021-07-04 10:31:36 +0100

    Changes to index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { useAuth } from "../auth";
 
 export default function Home() {
+  const { user } = useAuth();
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome to the home page</h1>
+      <h2 className={styles.description}>{`user ID: ${
+        user ? user.uid : "No user signed in"
+      }`}</h2>
+      {user ? (
+        <Link href="/authenticated">
+          <a>Go to authenticated route</a>
+        </Link>
+      ) : (
+        <Link href="/login">
+          <a>Login</a>
+        </Link>
+      )}
     </div>
   );
 }
