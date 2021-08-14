a51dee48f933dec2eca90b4ff7fc799bcf6aa668
Thomas Pritchard <thomaspritchard137@btinternet.com>
2021-08-14 16:45:25 +0100

    Follow button updates

--- pages/authenticated/profile/[slug].js
+++ pages/authenticated/profile/[slug].js
@@ -7,25 +7,75 @@ import PostItem from "@/components/PostItem";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import styles from "@/styles/SlugProfile.module.css";
+import router from "next/router";
 
-export default function SlugProfile({ session, posts, cookies, slugProfile }) {
+export default function SlugProfile({
+  session,
+  posts,
+  cookies,
+  slugProfile,
+  user,
+}) {
   firebaseClient();
 
   const follow = async () => {
-    const res = await fetch(`${API_URL}/user/follow/${slugProfile.username}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${cookies.token}`,
-      },
-    });
+    console.log(user.following);
+    console.log(
+      `${API_URL}/user/${
+        user.following.filter((follows) => {
+          return follows.user.toString() === slugProfile._id.toString();
+        }) === 0
+          ? "follow"
+          : "unfollow"
+      }/${slugProfile.username}`
+    );
+
+    console.log(
+      user.following.filter((follows) => {
+        console.log(follows.user.toString());
+        console.log(slugProfile._id.toString());
+        return follows.user.toString() === slugProfile._id.toString();
+      }).length === 0
+    );
+
+    const res = await fetch(
+      `${API_URL}/user/${
+        user.following.filter((follows) => {
+          return follows.user.toString() === slugProfile._id.toString();
+        }).length === 0
+          ? "follow"
+          : "unfollow"
+      }/${slugProfile.username}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cookies.token}`,
+        },
+      }
+    );
 
     const profile = await res.json();
 
     if (!res.ok) {
       toast.error(profile.msg);
+    } else {
+      router.reload();
     }
   };
+
+  // const following = async () => {
+  //   console.log(user);
+  //   if (
+  //     user.following.filter((follows) => {
+  //       return follows.user.toString() === slugProfile._id.toString();
+  //     }) === 0
+  //   ) {
+  //     return "notFollowing";
+  //   } else {
+  //     return "following";
+  //   }
+  // };
 
   if (session) {
     return (
@@ -35,13 +85,21 @@ export default function SlugProfile({ session, posts, cookies, slugProfile }) {
           <h1>{slugProfile.username}</h1>
           <div className={styles.right}>
             <div className={styles.follow}>
-              <h2>Followers: {slugProfile.followers.length}</h2>
+              <h2>Followersyeah: {slugProfile.followers.length}</h2>
               <h2>Following: {slugProfile.following.length}</h2>
             </div>
 
-            <button className="btn" onClick={follow}>
-              Follow
-            </button>
+            {user.following.filter((follows) => {
+              return follows.user.toString() === slugProfile._id.toString();
+            }).length === 0 ? (
+              <button className="btn" onClick={follow}>
+                Follow
+              </button>
+            ) : (
+              <button className="btnRed" onClick={follow}>
+                Unfollow
+              </button>
+            )}
           </div>
         </div>
 
@@ -84,12 +142,23 @@ export async function getServerSideProps(context) {
 
     const slugProfile = await res1.json();
 
+    const res2 = await fetch(`${API_URL}/profile/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.token}`,
+      },
+    });
+
+    const user = await res2.json();
+
     return {
       props: {
         session: `Your email is ${email} and your UID is ${uid}.`,
         posts,
         cookies,
         slugProfile,
+        user,
       },
     };
   } catch (err) {
