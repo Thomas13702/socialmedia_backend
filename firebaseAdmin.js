d71457aa0fac130eb28fc3403cee82257883fb45
Thomas Pritchard <thomaspritchard137@btinternet.com>
2021-08-28 12:07:05 +0100

    Changed auth to use .env variables

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -4,7 +4,18 @@ const serviceAccount = require("./socialmedia-auth-a034e-firebase-adminsdk-oucz7
 export const verifyIdToken = (token) => {
   if (!admin.apps.length) {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert({
+        type: process.env.type,
+        project_id: process.env.project_id,
+        private_key_id: process.env.private_key_id,
+        private_key: process.env.private_key,
+        client_email: process.env.client_email,
+        client_id: process.env.client_id,
+        auth_uri: process.env.auth_uri,
+        token_uri: process.env.token_uri,
+        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+        client_x509_cert_url: process.env.client_x509_cert_url,
+      }),
       databaseURL: process.env.databaseURL,
     });
   }
